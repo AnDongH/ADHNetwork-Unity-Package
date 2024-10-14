20f8518c6a45465797d137f6bb4ed07c7f8685a2
AnDongHyun <fblood53@haegin.kr>
2024-10-14 14:35:32 +0900

    test..

--- Runtime/testS.cs
+++ Runtime/testS.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ADHNetwork;
 
 public class testS : MonoBehaviour
 {
     private void Start() {
-        Debug.Log("test");
+
+        Debug.Log(ADHNetworkManager.ServerUrl);
     }
 }
