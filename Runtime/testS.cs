01ab0f8fd3f39d7963e7819dffea1cb52f419328
AnDongHyun <fblood53@haegin.kr>
2024-10-14 12:56:49 +0900

    test

---
+++ Runtime/testS.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testS : MonoBehaviour
+{
+    private void Start() {
+        Debug.Log("test");
+    }
+}
